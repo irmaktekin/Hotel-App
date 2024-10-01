e45969bdec4d1f1028f056f88301956016936992
irmaktekin <irmaktekin97@gmail.com>
2024-10-01 22:51:23 +0300

    Multiple thenReturn calls are implemented for testing with multiple inputs

--- src/test/java/com/mockitotutorial/happyhotel/booking/BookingServiceTest.java
+++ src/test/java/com/mockitotutorial/happyhotel/booking/BookingServiceTest.java
@@ -20,65 +20,83 @@ class BookingServiceTest {
     private MailSender mailSenderMock;
 
     @BeforeEach
-    void setup(){
+    void setup() {
         this.paymentServiceMock = mock(PaymentService.class);
         this.roomServiceMock = mock(RoomService.class);
         this.bookingDAOMock = mock(BookingDAO.class);
         this.mailSenderMock = mock(MailSender.class);
-        this.bookingService = new BookingService(paymentServiceMock,roomServiceMock,bookingDAOMock,mailSenderMock);
+        this.bookingService = new BookingService(paymentServiceMock, roomServiceMock, bookingDAOMock, mailSenderMock);
 
     }
 
     @Test
-    void should_CalculateCorrectPrice_When_CorrectInput(){
+    void should_CalculateCorrectPrice_When_CorrectInput() {
         //given
-        BookingRequest bookingRequest = new BookingRequest("1", LocalDate.of(2020,01,01),LocalDate.of(2020,01,05),2,false);
-        double expected = 4*2*50.0;
+        BookingRequest bookingRequest = new BookingRequest("1", LocalDate.of(2020, 01, 01), LocalDate.of(2020, 01, 05), 2, false);
+        double expected = 4 * 2 * 50.0;
 
         //when
-        double actual =  bookingService.calculatePrice(bookingRequest);
+        double actual = bookingService.calculatePrice(bookingRequest);
 
         //then
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void should_CountAvailablePlaces(){
+    void should_CountAvailablePlaces() {
         //given
-        int expected =0 ;
+        int expected = 0;
         //when
         int actual = bookingService.getAvailablePlaceCount();
 
         //then
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void shouldCountAvailablePlaces_When_OneRoomAvailable(){
+    void shouldCountAvailablePlaces_When_OneRoomAvailable() {
         //given
         when(this.roomServiceMock.getAvailableRooms())
-                .thenReturn(Collections.singletonList(new Room("Room 1",2)));
+                .thenReturn(Collections.singletonList(new Room("Room 1", 2)));
         int expected = 2;
         //when
         int actual = bookingService.getAvailablePlaceCount();
         //then
-        assertEquals(expected,actual);
-        }
+        assertEquals(expected, actual);
+    }
 
     @Test
-    void shouldCountAvailavlePlaces_When_MultipleRoomsAvailable(){
+    void shouldCountAvailavlePlaces_When_MultipleRoomsAvailable() {
         //given
-        List<Room> rooms = Arrays.asList(new Room("Room 1",2),new Room("Room 2",5));
+        List<Room> rooms = Arrays.asList(new Room("Room 1", 2), new Room("Room 2", 5));
         when(this.roomServiceMock.getAvailableRooms())
                 .thenReturn(rooms);
         int expected = 7;
         //when
         int actual = bookingService.getAvailablePlaceCount();
         //then
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
 
 
+    @Test
+    void shouldCountAvailablePlaces_When_CalledMultipleTimes() {
+        //given
+        when(this.roomServiceMock.getAvailableRooms())
+                .thenReturn(Collections.singletonList(new Room("Room 1",5)))
+                .thenReturn(Collections.emptyList());
+        int expectedFirstCall= 5;
+        int expectedSecondCall = 0;
+        //when
+        int actualFirst = bookingService.getAvailablePlaceCount();
+        int actualSecond = bookingService.getAvailablePlaceCount();
+
+        //then
+        assertAll(
+                ()->assertEquals(expectedFirstCall,actualFirst),
+                ()->assertEquals(expectedSecondCall,actualSecond)
+        );
 
 
+    }
 }
